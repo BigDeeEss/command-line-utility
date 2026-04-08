f6a1d43745c7f4b03cf1a54ae20854472f8d5d4d
Damon Swatton [A10] <damon.swatton@gmail.com>
2026-04-08 17:25:28 +0100

    Not working! ToDo: need to complete adding the CommandLineUtilityLib library so that GoogleTest can link to it for testing.

--- tst/tst_banner.cpp
+++ tst/tst_banner.cpp
@@ -1,7 +1,11 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions)
+#include <banner.hpp>
+
+TEST(BannerTest, CorrectOutput)
 {
+    EXPECT_STREQ(banner().str().c_str(), "Welcome to the Command Line Utility!\n");
+
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
