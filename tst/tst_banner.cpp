f9a6cbd5ae0bb32586831722c492f419a1e28568
Damon Swatton [A10] <damon.swatton@gmail.com>
2026-04-09 08:12:28 +0100

    Fix previous CMake error.
    Update tst_banner.cpp and ensure that unit tests execute correctly.

--- tst/tst_banner.cpp
+++ tst/tst_banner.cpp
@@ -1,13 +1,12 @@
+/// @file tst_banner.cpp
+/// @brief Unit test for banner function in banner.hpp
 #include <gtest/gtest.h>
 
 #include <banner.hpp>
 
+/// @brief Test that the banner function returns the expected output     
 TEST(BannerTest, CorrectOutput)
 {
+    // Call the banner function and check its output
     EXPECT_STREQ(banner().str().c_str(), "Welcome to the Command Line Utility!\n");
-
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
 }
